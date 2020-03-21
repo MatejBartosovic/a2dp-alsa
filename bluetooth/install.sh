070a74b55dbe623cd40a1e161883962bc26a4173
Matej Bartosovic <matejbart@gmail.com>
2020-03-21 20:13:06 +0100

    Refactor

--- bluetooth/install.sh
+++ bluetooth/install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 err_report() {
-    echo "Error on line $1"
+    echo "Error on line $2"
     exit 1
 }
 trap 'err_report "${BASH_SOURCE}" "${LINENO}"' ERR
-./compile_bluez.sh
+
+sudo apt install -y bluez bluez-tools bluealsa
+
 sudo cp -r config/* /etc/bluetooth
 
-#sudo sudo systemctl restart bluetooth.service
-
-sudo cp services/bt-agent.service /etc/systemd/system
-sudo cp services/blue-alsa.service /etc/systemd/system
-sudo cp services/blue-alsa-aplay.service /etc/systemd/system
+sudo cp services/bluealsa.service /lib/systemd/system/
+sudo cp services/bt-agent.service /etc/systemd/system/
+sudo cp services/blue-alsa-aplay.service /etc/systemd/system/
 
 cd udev
 ./install.sh
@@ -19,8 +19,11 @@ cd ..
 
 sudo systemctl daemon-reload
 
-sudo sudo systemctl enable bt-agent.service
-sudo sudo systemctl enable blue-alsa.service
+sudo systemctl enable bt-agent.service
+sudo systemctl enable bluealsa.service
+sudo systemctl enable blue-alsa-aplay.service
 
-sudo sudo systemctl start bt-agent.service
-sudo sudo systemctl start blue-alsa.service
+sudo systemctl restart bluetooth.service
+sudo systemctl start bt-agent.service
+sudo systemctl start bluealsa.service
+sudo systemctl start blue-alsa-aplay.service
